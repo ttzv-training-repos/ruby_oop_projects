70d7b2d59eeaffa2d8b8116edd1bfe85125549b2
ttzv <teyed1fy@gmail.com>
2020-05-10 18:49:11 +0200

    Initial commit

---
+++ tictactoe/tictactoe.rb
@@ -0,0 +1,13 @@
+class Board
+  
+  def initialize(size=3)
+    if size < 3 || t 
+      size = 3
+      puts "Incorrect value, creating Board with default size: #{size}"
+    end
+    @size = size
+  end
+
+
+
+end
